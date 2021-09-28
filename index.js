67dd366bfb3a3c2897a2dc1c31ca49ec9f368a12
Hazelton Chrome <rdupont1623@gmail.com>
2021-09-28 14:29:23 -0400

    Initial Outline Made as in ReadME - I have the basics of the classes sorted out - Now thinking about how to create HTML using path

---
+++ index.js
@@ -0,0 +1,8 @@
+const Build = require("./src/Build");
+
+// Initializing creation of new Employee Portfolio Object
+const build = new Build();
+
+// Begin making Portfolio
+build.create();
+
---
+++ src/Build.js
@@ -0,0 +1,3 @@
+/* The framework for the creation of the new file
+is going here --- I've decided the function that 
+*/
